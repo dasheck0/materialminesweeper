9394f6f7716d9149940703b75cd14d7c00d1e083
Stefan Neidig <s.neidig@appcom-interactive.de>
2016-01-24 12:06:12 +0100

    Add more flag asset sizes

--- app/src/main/java/com/dasheck/materialminesweeper/adapters/TileListAdapter.java
+++ app/src/main/java/com/dasheck/materialminesweeper/adapters/TileListAdapter.java
@@ -47,7 +47,7 @@ public class TileListAdapter extends BaseAdapter<Tile, TileListAdapter.ViewHolde
     } else {
       if (item.isMarked()) {
         holder.rootLayout.setBackground(Utilities.colorDrawable(context,
-            context.getResources().getDrawable(R.drawable.ic_flag_variant_grey600_24dp),
+            context.getResources().getDrawable(R.drawable.ic_flag_variant_grey600_48dp),
             context.getResources().getColor(R.color.colorAccent)));
       } else {
         holder.rootLayout.setBackground(
